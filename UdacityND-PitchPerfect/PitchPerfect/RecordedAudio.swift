912be96af1bb3d1df7df8f762bda7c094c989c88
Andrew Park <andrew.park@ntlworld.com>
2015-03-18 12:37:09 +0000

    Committed by Udacity

---
+++ UdacityND-PitchPerfect/PitchPerfect/RecordedAudio.swift
@@ -0,0 +1,16 @@
+//
+//  RecordedAudio.swift
+//  PitchPerfect
+//
+//  Created by andrew on 16/03/2015.
+//  Copyright (c) 2015 Firekite. All rights reserved.
+//  Taken from Udacity NanaDegree COURSE 
+
+import Foundation
+
+
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
